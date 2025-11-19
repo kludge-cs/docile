8e1f2c2bf52462494fcfd6381c46428e75d45636
drainpixie <121581793+drainpixie@users.noreply.github.com>
2025-11-19 12:25:11 +0100

    refactor: use with_capacity for children

--- src/parser.rs
+++ src/parser.rs
@@ -64,7 +64,12 @@ impl Parsed {
 	/// Returns an iterator over the children of a node.
 	pub fn children<'a>(&self, node: Node<'a>) -> Vec<Node<'a>> {
 		let mut cursor = node.walk();
-		node.children(&mut cursor).collect()
+
+		let children = node.children(&mut cursor);
+		let mut result = Vec::with_capacity(children.len());
+
+		result.extend(children);
+		result
 	}
 
 	/// Returns the text content of a node.
